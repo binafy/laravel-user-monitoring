3b7dc5d5b5811e9c52956996d01376f486f4be7e
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-23 02:25:00 +0330

    init primitive files

--- src/LaravelUserMonitoringServiceProvider.php
+++ src/LaravelUserMonitoringServiceProvider.php
@@ -2,7 +2,34 @@
 
 namespace Binafy\LaravelUserMonitoring;
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
 class LaravelUserMonitoringServiceProvider extends ServiceProvider
 {
+    /**
+     * Register files.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views/', 'LaravelUserMonitoring');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->mergeConfigFrom(__DIR__ . '/../config/user-monitoring.php', 'user-monitoring');
 
+        Route::middleware('web')->group(__DIR__ . '/../routes/web.php');
+
+        // TODO: Add publish
+    }
+
+    /**
+     * Boot provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
 }
