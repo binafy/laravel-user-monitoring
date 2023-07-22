db4730939b2a03dda4f18b228ff43cafb44f4db3
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-23 03:07:02 +0330

    install `jenssegers/agent`

--- src/Middlewares/MonitorVisitMiddleware.php
+++ src/Middlewares/MonitorVisitMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace Binafy\LaravelUserMonitoring\Middlewares;
 
+use App\Models\VisitMonitoring;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -17,6 +18,15 @@ class MonitorVisitMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        // TODO: read guard for user from somewhere
+        VisitMonitoring::query()->create([
+            'user_id' => auth()->id(),
+            'browse_name' => ,
+            'platform' => ,
+            'device' => $request->userAgent(),
+            'ip' => $request->ip(),
+            'page' => $request->url(),
+        ]);
 
         return $next($request);
     }
