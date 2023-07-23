b2126e0150cd25d0df28af5182df2b2bb4332377
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-23 18:28:24 +0330

    add ActionMonitoring with migration

---
+++ src/Models/ActionMonitoring.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ActionMonitoring extends Model
+{
+    use HasFactory;
+
+    /**
+     * Set table name.
+     *
+     * @var string
+     */
+    protected $table = 'actions_monitoring';
+}
