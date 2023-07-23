04c837da1580bbf6911d79e8f3e07ad695a77a22
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-23 19:37:54 +0330

    add `update action monitoring when a model created with login user` test

--- tests/Feature/ActionMonitoringTest.php
+++ tests/Feature/ActionMonitoringTest.php
@@ -48,3 +48,24 @@ test('store action monitoring when a model created without login user', function
     assertDatabaseCount(config('user-monitoring.action_monitoring.table'), 1);
     assertDatabaseHas(config('user-monitoring.action_monitoring.table'), ['page' => url('/')]);
 });
+
+test('update action monitoring when a model created with login user', function () {
+    $user = createUser();
+    auth()->login($user);
+
+    $product = Product::query()->create([
+        'title' => 'milwad'
+    ]);
+    $product->update(['title' => 'Binafy']);
+
+    // Assertions
+    expect(ActionMonitoring::query()->value('table_name'))
+        ->toBe('products')
+        ->and(ActionMonitoring::query()->where('id', 2)->value('action_type'))
+        ->toBe(ActionEnum::ACTION_UPDATE->value)
+        ->and($user->name)->toBe(ActionMonitoring::first()->user->name);
+
+    // DB Assertions
+    assertDatabaseCount(config('user-monitoring.action_monitoring.table'), 2);
+    assertDatabaseHas(config('user-monitoring.action_monitoring.table'), ['page' => url('/')]);
+});
