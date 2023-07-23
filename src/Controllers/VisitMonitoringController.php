0c5ebb8ba3d6fef23fb8aabbc18ed5b0c07bee06
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-23 20:07:13 +0330

    Update VisitMonitoringController.php

--- src/Controllers/VisitMonitoringController.php
+++ src/Controllers/VisitMonitoringController.php
@@ -2,10 +2,14 @@
 
 namespace Binafy\LaravelUserMonitoring\Controllers;
 
+use Binafy\LaravelUserMonitoring\Models\VisitMonitoring;
+
 class VisitMonitoringController extends BaseController
 {
     public function index()
     {
-        return view('LaravelUserMonitoring::visit-monitoring.index');
+        $visits = VisitMonitoring::query()->latest()->get();
+
+        return view('LaravelUserMonitoring::visit-monitoring.index', compact('visits'));
     }
 }
