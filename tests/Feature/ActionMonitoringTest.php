d2d714a6122cdc747976cc01a82fc592f09b4527
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-23 19:30:02 +0330

    complete `store action monitoring when a model created with login user` test

--- tests/Feature/ActionMonitoringTest.php
+++ tests/Feature/ActionMonitoringTest.php
@@ -1,7 +1,11 @@
 <?php
 
+use Binafy\LaravelUserMonitoring\Enums\ActionEnum;
+use Binafy\LaravelUserMonitoring\Models\ActionMonitoring;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\SetUp\Models\Product;
+
+use function Pest\Laravel\{assertDatabaseCount, assertDatabaseHas};
 
 /*
  * Use `RefreshDatabase` for delete migration data for each test.
@@ -10,8 +14,20 @@ uses(RefreshDatabase::class);
 
 test('store action monitoring when a model created with login user', function () {
     $user = createUser();
+    auth()->login($user);
 
     Product::query()->create([
          'title' => 'milwad'
     ]);
+
+    // Assertions
+    expect(ActionMonitoring::query()->value('table_name'))
+        ->toBe('products')
+        ->and(ActionMonitoring::query()->value('action_type'))
+        ->toBe(ActionEnum::ACTION_STORE->value)
+        ->and($user->name)->toBe(ActionMonitoring::first()->user->name);
+
+    // DB Assertions
+    assertDatabaseCount(config('user-monitoring.action_monitoring.table'), 1);
+    assertDatabaseHas(config('user-monitoring.action_monitoring.table'), ['page' => url('/')]);
 });
