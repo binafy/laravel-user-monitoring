ccfb62ebf30b57e3e0272ade44332034fe35ca74
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-23 20:16:26 +0330

    add actions monitoring routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use Binafy\LaravelUserMonitoring\Controllers\VisitMonitoringController;
+use Binafy\LaravelUserMonitoring\Controllers\ActionMonitoringController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('user-monitoring')->as('user-monitoring.')->group(function ($router) {
     // Visit Monitoring Routes
-    $router->get('visits-monitoring', [VisitMonitoringController::class, 'index'])->name('visit-monitoring');
-    $router->delete('visits-monitoring/{visitMonitoring}', [VisitMonitoringController::class, 'destroy'])->name('visit-monitoring-delete');
-});
+    $router->get('visits-monitoring', [VisitMonitoringController::class, 'index'])->name('visits-monitoring');
+    $router->delete('visits-monitoring/{visitMonitoring}', [VisitMonitoringController::class, 'destroy'])->name('visits-monitoring-delete');
 
+    // Action Monitoring Routes
+    $router->get('actions-monitoring', [ActionMonitoringController::class, 'index'])->name('actions-monitoring');
+    $router->delete('actions-monitoring/{visitMonitoring}', [ActionMonitoringController::class, 'destroy'])->name('actions-monitoring-delete');
+});
