e6dfe75afad969abb205efdc81ea17ba32ab43c6
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-23 20:20:44 +0330

    complete ActionMonitoringController

--- src/Controllers/ActionMonitoringController.php
+++ src/Controllers/ActionMonitoringController.php
@@ -2,7 +2,22 @@
 
 namespace Binafy\LaravelUserMonitoring\Controllers;
 
+use Binafy\LaravelUserMonitoring\Models\ActionMonitoring;
+
 class ActionMonitoringController extends BaseController
 {
+    public function index()
+    {
+        $actions = ActionMonitoring::query()->latest()->get();
 
+        return view('LaravelUserMonitoring::actions-monitoring.index', compact('actions'));
+    }
+
+    public function destroy(ActionMonitoring $actionMonitoring)
+    {
+        $actionMonitoring->delete();
+
+        // TODO: Add alert
+        return to_route('user-monitoring.actions-monitoring');
+    }
 }
