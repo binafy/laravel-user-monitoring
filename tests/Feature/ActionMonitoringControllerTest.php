175ecc2573195c5949671a513fe413af02edfbd6
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-23 20:23:38 +0330

    Create ActionMonitoringControllerTest.php

---
+++ tests/Feature/ActionMonitoringControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use function Pest\Laravel\get;
+
+/*
+ * Use `RefreshDatabase` for delete migration data for each test.
+ */
+uses(RefreshDatabase::class);
+
