3d928dfaa07de888f352b7c5792b1c3177a4edd4
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-25 17:21:09 +0330

    add `when user deleted authentications-monitoring rows were deleted` test

--- tests/Feature/AuthenticationMonitoringTest.php
+++ tests/Feature/AuthenticationMonitoringTest.php
@@ -3,7 +3,7 @@
 use Binafy\LaravelUserMonitoring\Models\AuthenticationMonitoring;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\SetUp\Models\User;
-use function Pest\Laravel\{assertDatabaseCount, assertDatabaseHas};
+use function Pest\Laravel\{assertDatabaseCount, assertDatabaseHas, assertDatabaseMissing};
 
 /*
  * Use `RefreshDatabase` for delete migration data for each test.
@@ -57,6 +57,20 @@ test('when user deleted authentications-monitoring rows didnt deleted', function
     // DB Assertions
     assertDatabaseCount(config('user-monitoring.authentication_monitoring.table'), 1);
     assertDatabaseHas(config('user-monitoring.authentication_monitoring.table'), [
+        'user_id' => 1,
+        'action_type' => 'login',
+    ]);
+});
+
+test('when user deleted authentications-monitoring rows were deleted', function () {
+    $user = createUser();
+    auth()->login($user);
+
+    User::query()->first()->delete();
+
+    // DB Assertions
+    assertDatabaseCount(config('user-monitoring.authentication_monitoring.table'), 0);
+    assertDatabaseMissing(config('user-monitoring.authentication_monitoring.table'), [
         'user_id' => 1,
         'action_type' => 'login',
     ]);
