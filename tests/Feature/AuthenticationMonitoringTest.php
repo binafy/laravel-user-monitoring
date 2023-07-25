fdfd22f2a9aa7214d69ad92617a0c6915c233a68
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-25 17:17:21 +0330

    add some assertations for AuthenticationMonitoringTest

--- tests/Feature/AuthenticationMonitoringTest.php
+++ tests/Feature/AuthenticationMonitoringTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Binafy\LaravelUserMonitoring\Models\AuthenticationMonitoring;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use function Pest\Laravel\{assertDatabaseCount, assertDatabaseHas};
 
@@ -11,6 +12,12 @@ uses(RefreshDatabase::class);
 test('store authentication monitoring when a user login', function () {
     $user = createUser();
     auth()->login($user);
+
+    // Assertions
+    expect($user->name)
+        ->toBe(AuthenticationMonitoring::first()->user->name)
+        ->and('login')
+        ->toBe(AuthenticationMonitoring::first()->value('action_type'));
 
     // DB Assertions
     assertDatabaseCount(config('user-monitoring.authentication_monitoring.table'), 1);
@@ -24,6 +31,12 @@ test('store authentication monitoring when a user logout', function () {
     $user = createUser();
     auth()->login($user);
     auth()->logout();
+
+    // Assertions
+    expect($user->name)
+        ->toBe(AuthenticationMonitoring::first()->user->name)
+        ->and('logout')
+        ->toBe(AuthenticationMonitoring::query()->firstWhere('id', 2)->action_type);
 
     // DB Assertions
     assertDatabaseCount(config('user-monitoring.authentication_monitoring.table'), 2);
