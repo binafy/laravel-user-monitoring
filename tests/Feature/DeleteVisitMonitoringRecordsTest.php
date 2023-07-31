6f6d949834e72a83b6813b59980256f9deaca1b5
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-31 14:23:34 +0330

    add `rows are not deleted after 1 day` test

--- tests/Feature/DeleteVisitMonitoringRecordsTest.php
+++ tests/Feature/DeleteVisitMonitoringRecordsTest.php
@@ -11,7 +11,7 @@ use function Pest\Laravel\assertDatabaseMissing;
  */
 uses(RefreshDatabase::class);
 
-test('rows are delete by 1 days', function () {
+test('rows are delete after 1 days', function () {
     // Set delete days
     config()->set('user-monitoring.visit_monitoring.delete_days', 1);
 
@@ -43,4 +43,38 @@ test('rows are delete by 1 days', function () {
 
     // DB Assertions
     assertDatabaseCount(config('user-monitoring.visit_monitoring.table'), 0);
+});
+
+test('rows are not deleted after 1 day', function () {
+    // Set delete days
+    config()->set('user-monitoring.visit_monitoring.delete_days', 1);
+
+    // Store old records for 1 day age
+    $user = createUser();
+
+    DB::table(config('user-monitoring.visit_monitoring.table'))->insert([
+        [
+            'user_id' => $user->id,
+            'browser_name' => 'Chrome',
+            'platform' => 'Windows',
+            'device' => 'WebKit',
+            'ip' => '127.0.0.1',
+            'page' => 'http://localhost:8000',
+            'created_at' => now(),
+        ],
+        [
+            'user_id' => $user->id,
+            'browser_name' => 'Firefox',
+            'platform' => 'Linux',
+            'device' => 'WebKit',
+            'ip' => '123.12.531.11',
+            'page' => 'http://localhost:8000/milwad',
+            'created_at' => now(),
+        ]
+    ]);
+
+    artisan('laravel-user-monitoring:remove-visit-monitoring-records');
+
+    // DB Assertions
+    assertDatabaseCount(config('user-monitoring.visit_monitoring.table'), 2);
 });
