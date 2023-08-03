8437aac0e2aef0fb69073cb576ad26fd4ef9808b
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-08-03 18:24:24 +0330

    fix github address

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -45,7 +45,7 @@
                         </a>
                     </div>
                     <div>
-                        <a href="https://github.com/milwad-dev"
+                        <a href="https://github.com/binafy/laravel-user-monitoring"
                            class="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 mt-4 sm:mt-0
                                     inline-flex items-start justify-start px-6 py-3 bg-indigo-700 hover:bg-indigo-600
                                     focus:outline-none rounded items-center">
--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,15 @@ use Binafy\LaravelUserMonitoring\Controllers\VisitMonitoringController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('user-monitoring')->as('user-monitoring.')->group(function ($router) {
-    // Visit Monitoring Routes
+    // Visit Monitoring
     $router->get('visits-monitoring', [VisitMonitoringController::class, 'index'])->name('visits-monitoring');
     $router->delete('visits-monitoring/{visitMonitoring}', [VisitMonitoringController::class, 'destroy'])->name('visits-monitoring-delete');
 
-    // Action Monitoring Routes
+    // Action Monitoring
     $router->get('actions-monitoring', [ActionMonitoringController::class, 'index'])->name('actions-monitoring');
     $router->delete('actions-monitoring/{actionMonitoring}', [ActionMonitoringController::class, 'destroy'])->name('actions-monitoring-delete');
 
-    // Authentication Monitoring Routes
+    // Authentication Monitoring
     $router->get('authentications-monitoring', [AuthenticationMonitoringController::class, 'index'])->name('authentications-monitoring');
     $router->delete('authentications-monitoring/{authenticationMonitoring}', [AuthenticationMonitoringController::class, 'destroy'])->name('authentications-monitoring-delete');
 });
