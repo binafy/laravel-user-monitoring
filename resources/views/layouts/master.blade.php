e76c2dbb6bef22856ecd960e4e234787ed76df12
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-08-03 19:55:25 +0330

    add chart emoji

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,7 +19,7 @@
             <div class="px-4 md:px-10 py-4 md:py-7">
                 <div class="flex items-center justify-between">
                     <p class="focus:outline-none text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-gray-800" tabindex="0">
-                        {{ $title }}
+                        {{ $title }} 📈
                     </p>
                 </div>
             </div>
