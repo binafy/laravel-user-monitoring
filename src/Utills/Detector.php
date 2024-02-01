97aa5a5f5d422a18625c33d6afdffc6cd0e2a702
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2024-02-01 12:31:54 +0330

    add new device names

--- src/Utills/Detector.php
+++ src/Utills/Detector.php
@@ -26,10 +26,29 @@ class Detector
      * @var array
      */
     public array $deviceName = [
-        '/iPhone/i'           => 'iPhone',
-        '/iPad/i'             => 'iPad',
-        '/Android/i'          => 'Android Device',
-        '/Windows/i'          => 'Windows',
+        '/windows nt 10/i'      =>  'Windows 10',
+        '/windows nt 6.3/i'     =>  'Windows 8.1',
+        '/windows nt 6.2/i'     =>  'Windows 8',
+        '/windows nt 6.1/i'     =>  'Windows 7',
+        '/windows nt 6.0/i'     =>  'Windows Vista',
+        '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+        '/windows nt 5.1/i'     =>  'Windows XP',
+        '/windows xp/i'         =>  'Windows XP',
+        '/windows nt 5.0/i'     =>  'Windows 2000',
+        '/windows me/i'         =>  'Windows ME',
+        '/win98/i'              =>  'Windows 98',
+        '/win95/i'              =>  'Windows 95',
+        '/win16/i'              =>  'Windows 3.11',
+        '/macintosh|mac os x/i' =>  'Mac OS X',
+        '/mac_powerpc/i'        =>  'Mac OS 9',
+        '/linux/i'              =>  'Linux',
+        '/ubuntu/i'             =>  'Ubuntu',
+        '/iphone/i'             =>  'iPhone',
+        '/ipod/i'               =>  'iPod',
+        '/ipad/i'               =>  'iPad',
+        '/android/i'            =>  'Android',
+        '/blackberry/i'         =>  'BlackBerry',
+        '/webos/i'              =>  'Mobile'
     ];
 
     /**
