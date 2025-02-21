ee21a6c247515dee155bcd744563e3792b615801
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-02-21 14:11:19 +0330

    Update user-monitoring.php

--- config/user-monitoring.php
+++ config/user-monitoring.php
@@ -90,7 +90,7 @@ return [
          * Set to 0 to disable automatic deletion.
          *
          * To enable automatic deletion, configure Laravel's task scheduling as outlined here:
-         * https://laravel.com/docs/10.x/scheduling
+         * https://laravel.com/docs/scheduling
          */
         'delete_days' => 0,
     ],
