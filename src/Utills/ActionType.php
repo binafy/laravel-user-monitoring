0dc2637f6cd536c0b706acea1e035ad96b058b0d
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-02-21 13:54:59 +0330

    Update ActionType.php

--- src/Utills/ActionType.php
+++ src/Utills/ActionType.php
@@ -10,4 +10,13 @@ class ActionType
     public const ACTION_READ = 'read';
     public const ACTION_RESTORED = 'restore';
     public const ACTION_REPLICATE = 'replicate';
+
+    public static array $types = [
+        self::ACTION_STORE,
+        self::ACTION_UPDATE,
+        self::ACTION_DELETE,
+        self::ACTION_READ,
+        self::ACTION_RESTORED,
+        self::ACTION_REPLICATE,
+    ];
 }
