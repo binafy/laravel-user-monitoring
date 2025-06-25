3bc2c0dd11e2c54cd605947c00a9a0d8b1cc21dd
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-06-25 13:54:29 +0330

    add `action stored when the guest mode is off and user logged in` test

--- tests/Feature/ActionMonitoringTest.php
+++ tests/Feature/ActionMonitoringTest.php
@@ -329,3 +329,19 @@ test('action stored when the guest mode is on and user not logged in', function 
     // DB Assertions
     assertDatabaseCount(config('user-monitoring.action_monitoring.table'), 2);
 });
+
+test('action stored when the guest mode is off and user logged in', function () {
+    config()->set('user-monitoring.action_monitoring.guest_mode', false);
+
+    $user = createUser();
+    auth()->login($user);
+
+    $product = Product::query()->create([
+        'title' => 'milwad',
+        'description' => 'WE ARE HELPING TO OPEN-SOURCE WORLD'
+    ]);
+    $product->delete();
+
+    // DB Assertions
+    assertDatabaseCount(config('user-monitoring.action_monitoring.table'), 2);
+});
