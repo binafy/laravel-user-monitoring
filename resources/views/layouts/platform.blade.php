3a78541a2b1a01e64151d22f57faaf5cf7659e32
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-09-09 22:06:54 +0330

    Create platform.blade.php

---
+++ resources/views/layouts/platform.blade.php
@@ -0,0 +1,5 @@
+@if (strtolower(str_contains($platform)) == 'windows')
+    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" viewBox="0 0 24 24">
+        <path fill="currentColor" fill-rule="evenodd" d="M3.005 12 3 6.408l6.8-.923v6.517H3.005ZM11 5.32 19.997 4v8H11V5.32ZM20.067 13l-.069 8-9.065-1.275L11 13h9.067ZM9.8 19.58l-6.795-.931V13H9.8v6.58Z" clip-rule="evenodd"/>
+    </svg>
+@endif
